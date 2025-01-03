c4cba585ca58f66786f20a2a72eef00bb8dbe9a4
Nvuala Carvalho <81969053+Carva47@users.noreply.github.com>
2025-01-03 15:12:08 +0100

    shpgal

---
+++ carrsolBG.js
@@ -0,0 +1,19 @@
+
+        const slides = document.querySelectorAll('#carousel > div[data-index]');
+        let currentIndex = 0;
+      
+        function showNextSlide() {
+          slides[currentIndex].classList.remove('opacity-100');
+          slides[currentIndex].classList.add('opacity-0');
+          currentIndex = (currentIndex + 1) % slides.length;
+          slides[currentIndex].classList.remove('opacity-0');
+          slides[currentIndex].classList.add('opacity-100');
+        }
+      
+        setInterval(showNextSlide, 5000); // Troca de slide a cada 5 segundos
+
+        // Animações de scroll AOS.init
+        AOS.init({
+          duration: 1000, // Duração da animação em milissegundos
+             once: false,     // Executar apenas uma vez
+         });
